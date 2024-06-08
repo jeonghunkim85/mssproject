8769f81018a4a55044db796824e0b71cbc7202ac
jeonghunkim85 <shockshot@naver.com>
2024-06-08 15:14:46 +0900

    Initial commit

---
+++ src/main/kotlin/com/mss/mssproject/MssprojectApplication.kt
@@ -0,0 +1,11 @@
+package com.mss.mssproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MssprojectApplication
+
+fun main(args: Array<String>) {
+    runApplication<MssprojectApplication>(*args)
+}
---
+++ src/test/kotlin/com/mss/mssproject/MssprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mss.mssproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MssprojectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
