b90fa044f6521ac3ed6e9224c6509df6ff01458f
jeonghunkim85 <shockshot@naver.com>
2024-06-12 00:17:04 +0900

    fix application load test

--- src/test/kotlin/com/mss/mssproject/MssprojectApplicationTests.kt
+++ src/test/kotlin/com/mss/mssproject/MssprojectApplicationTests.kt
@@ -3,11 +3,16 @@ package com.mss.mssproject
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(
+    // 전체 테스트 동시 수행시 db initializing 관련 문제가 발생하여 임시조치
+    properties = [
+        "spring.sql.init.mode=never",
+        "spring.jpa.hibernate.ddl-auto=none"
+    ]
+)
 class MssprojectApplicationTests {
 
     @Test
     fun contextLoads() {
     }
-
 }
